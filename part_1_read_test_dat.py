856e83c4ce54368177b1a79745de1f601ed82f52
elugo1 <82634063+elugo1@users.noreply.github.com>
2024-02-11 16:05:50 -0500

    part_1_read_test_dat_
    completion of HW part 1, turning a dat into a text file

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -1,8 +1,7 @@
 import cc_dat_utils
-
 #Part 1
 input_dat_file = "data/pfgd_test.dat"
 
+level_pack = cc_dat_utils.make_cc_level_pack_from_dat(input_dat_file)
+print(level_pack)
 
-#Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file
-#print the resulting data

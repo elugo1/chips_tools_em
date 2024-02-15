92ced6fd9536e9241c5cb75b398febd286f0e290
elugo1 <82634063+elugo1@users.noreply.github.com>
2024-02-14 20:19:44 -0500

    part2
    this one was really really hard for me! I think the for-loop structure in python really threw me for a loop

--- part_2_read_test_json.py
+++ part_2_read_test_json.py
@@ -1,20 +1,29 @@
 import test_data
 import json
 
+
+
 #Creates and returns a GameLibrary object(defined in test_data) from loaded json_data
 def make_game_library_from_json( json_data ):
     #Initialize a new GameLibrary
     game_library = test_data.GameLibrary()
 
     ### Begin Add Code Here ###
-    #Loop through the json_data
+    for game_value, game_value in json_data.items():
         #Create a new Game object from the json_data by reading
-        #  title
-        #  year
-        #  platform (which requires reading name and launch_year)
-        #Add that Game object to the game_library
-    ### End Add Code Here ###
+        title = game_value["title"]
+        year = game_value["year"]
 
+        platform_data = game_value["platform"]
+        name = platform_data["name"]
+        launch_year = platform_data["launch year"]  
+        
+        #  platform (which requires reading name and launch_year)
+        platform = test_data.Platform(name,launch_year)
+        game_object = test_data.Game(title,platform,year)
+        #Add that Game object to the game_library
+        game_library.add_game(game_object)
+    ### End Add Code Here ### 
     return game_library
 
 
@@ -22,8 +31,10 @@ def make_game_library_from_json( json_data ):
 input_json_file = "data/test_data.json"
 
 ### Begin Add Code Here ###
-#Open the file specified by input_json_file
-#Use the json module to load the data from the file
-#Use make_game_library_from_json(json_data) to convert the data to GameLibrary data
-#Print out the resulting GameLibrary data using print()
+with open(input_json_file, "r") as reader:
+    #load the JSON data and store it in the variable game_json_data
+    json_data = json.load(reader)
+    #Use the json module to load the data from the file
+    game_lib_data = make_game_library_from_json(json_data)
+print(game_lib_data)
 ### End Add Code Here ###

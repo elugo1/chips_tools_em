a43fc8d0e42b5f76ec24004cfade55e2b6eb59f8
elugo1 <82634063+elugo1@users.noreply.github.com>
2024-03-03 21:17:09 -0500

    a better editor push

--- part_3_convert_json.py
+++ part_3_convert_json.py
@@ -4,7 +4,7 @@ import json
 
 #Part 3
 #Load your custom JSON file
-input_json_file = "data/elugo_cc1.json"
+input_json_file = "data/elugo_cc_level_pack.json"
 
 
 #Convert JSON data to CCLevelPack
@@ -12,25 +12,20 @@ input_json_file = "data/elugo_cc1.json"
 def convert_json_to_CCLevelPack(json_data):
     level_pack = CCLevelPack()
 
-    levels_data = json_data['Levels'] 
-    level = CCLevel()
-    level.level_number = levels_data['level_num']
-    level.time = levels_data['time']
-    level.num_chips = levels_data["chip number"]
-    fields = levels_data["fields"]
-    for field_name, field_value in fields.items():
-        if field_name == "field 3":
-            level.add_field(CCMapTitleField(field_value))
-        elif field_name == "field 6":
-            level.add_field(CCEncodedPasswordField(field_value))
-        elif field_name == "field 7":
-            level.add_field(CCMapHintField(field_value))
-        elif field_name == "field 10":
-            monster_coordinates = [CCCoordinate(field_value[0],field_value[1])]
-            level.add_field(CCMonsterMovementField(monster_coordinates))
-    upper_layer = json_data["upper layer"]
-    level.upper_layer = upper_layer[:1024]
-    level_pack.add_level(level)
+    levels_data = json_data['levels'] 
+    for i, level_json in enumerate(levels_data):
+        level = CCLevel() 
+        level.level_number = i #level_json['level_num']
+        level.time = level_json['time']
+        level.num_chips = level_json["numberOfChips"]
+        password_str = level_json['password']
+        password_list = [int(part) for part in password_str.split(',')]
+        level.add_field(CCEncodedPasswordField(password_list))
+        level.add_field(CCMapTitleField(level_json["title"]))
+        level.add_field(CCMapHintField(level_json["hintText"]))
+        level.upper_layer = level_json["upperLayer"]
+    # level.upper_layer = upper_layer[2:10]
+        level_pack.add_level(level)
     
     return level_pack
 
@@ -43,4 +38,4 @@ with open(input_json_file, "r") as reader:
 #Save converted data to DAT file
 if __name__ == "__main__":
     LevelPack = convert_json_to_CCLevelPack(json_data)
-    cc_dat_utils.write_cc_level_pack_to_dat(LevelPack,'.\data\elugo_cc1.dat')
+    cc_dat_utils.write_cc_level_pack_to_dat(LevelPack,'.\data\elugo_cc_level.dat')
